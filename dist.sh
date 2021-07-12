e1f06986efb70fde7a66b8e936561639d47344ed
Cameron Manderson <cam@alpaca.travel>
2021-07-12 12:06:48 +1000

    Updates the dist to touch the next version

--- dist.sh
+++ dist.sh
@@ -7,7 +7,7 @@ if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
   echo "Deploying"
   aws s3 cp ./dist s3://cdn.alpacamaps.com/scripts/alpaca-toolkit/$PACKAGE_VERSION/$REV --recursive
   aws s3 cp ./docs s3://developer.alpacamaps.com --recursive
-
+  touch ./dist/alpaca-toolkit@next.js
   aws s3 cp ./dist/alpaca-toolkit@next.js s3://cdn.alpacamaps.com/scripts/ --website-redirect /scripts/alpaca-toolkit/$PACKAGE_VERSION/$REV/alpaca-toolkit.js
   echo "Note: You will need to update the website redirects in cdn.alpacamaps.com to point to this release manually"
 else
